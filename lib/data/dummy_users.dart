a31526fdf232d0621102746eb81d6b1e287108b1
Suleiman Moraes <suleimanmoraes@gmail.com>
2020-07-20 19:21:13 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/model/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/01/02/10/17/alien-1946417_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2019/06/17/22/21/capt-america-4281073_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2020/04/08/02/45/cartoon-5015515_960_720.png'),
+  '4': const User(
+      id: '4',
+      name: 'Guilherme',
+      email: 'guigui@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/04/01/10/04/amusing-1299762_960_720.png'),
+};
